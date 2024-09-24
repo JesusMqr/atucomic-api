9cc6cef561b4bd5ed724ba689646419cf545b1ee
Jesus Maquera <jesusmaquera10@gmail.com>
2024-09-23 23:50:32 -0500

    feat: add public, logout and profile routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,23 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\Content\ChapterController;
+use App\Http\Controllers\Api\Content\SerieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login',[AuthController::class,'login']);
 Route::post('register',[AuthController::class,'register']);
+Route::prefix('public')->group(function(){
+    Route::get('series',[SerieController::class,'index']);
+    Route::get('series/search',[SerieController::class,'search']);
+    Route::get('series/{series}',[SerieController::class,'show']);
+    Route::get('chapters/{chapter}',[ChapterController::class,'show']);
+
+});
 
 Route::middleware('auth:sanctum')->group(function(){
+    Route::post('logout',[AuthController::class,'logout']);
+    Route::get('profile',[AuthController::class,'profile']);
     require __DIR__.'/api_v1.php';
 });
