677e7a5482d8e1bcd082160b3e7bae6247b6d344
Jesus Maquera <jesusmaquera10@gmail.com>
2024-09-30 09:28:04 -0500

    feat: add UserProfileController

---
+++ app/Http/Controllers/API/User/UserProfileController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\ProfileResource;
+use Illuminate\Support\Facades\Http;
+
+class UserProfileController extends Controller
+{
+    public function Profile(Request $request)
+    {
+        $user = $request->user();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json(new ProfileResource($user),Response::HTTP_OK);
+    }
+
+    public function updateProfile(Request $request){
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $request->user()->id,
+        ]);
+
+        $user = $request->user();
+        $user->update($request->only(['name', 'email'])); // Asegúrate de que solo se actualicen los campos correctos.
+
+
+        return response()->json([
+            'message' => 'Profile updated successfully',
+            'user' => $user
+        ], Response::HTTP_OK);
+
+    }
+}
